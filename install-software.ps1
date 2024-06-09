bdf89255afbdf914dad69fa9eac1d320f0af8831
pigilams <48093287+pigilams@users.noreply.github.com>
2024-06-09 23:55:45 +0530

    Update install-software.ps1

--- install-software.ps1
+++ install-software.ps1
@@ -12,17 +12,3 @@ Write-Output "Installing Azure CLI..."
 choco install azure-cli -y
 Write-Output "Azure CLI installation completed."
 
-# Install SQL*Plus
-Write-Output "Downloading Oracle Instant Client..."
-$oracleClientUrl = "https://download.oracle.com/otn_software/nt/instantclient/instantclient-basiclite-windows.x64-19.8.0.0.0dbru.zip"
-Invoke-WebRequest -Uri $oracleClientUrl -OutFile "C:\instantclient-basiclite-windows.x64.zip"
-Write-Output "Oracle Instant Client downloaded successfully."
-
-Write-Output "Expanding Oracle Instant Client archive..."
-Expand-Archive -Path "C:\instantclient-basiclite-windows.x64.zip" -DestinationPath "C:\Oracle"
-Write-Output "Oracle Instant Client archive expanded successfully."
-
-Write-Output "Updating system PATH..."
-$env:Path += ";C:\Oracle\instantclient_19_8"
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [EnvironmentVariableTarget]::Machine)
-Write-Output "System PATH updated."
