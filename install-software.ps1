06b96738460eff3e70c884c7a5b299720fa6bbb1
pigilams <48093287+pigilams@users.noreply.github.com>
2024-06-09 20:50:39 +0530

    Update install-software.ps1

--- install-software.ps1
+++ install-software.ps1
@@ -1,7 +1,6 @@
 # Install Chocolatey
 Write-Output "Installing Chocolatey..."
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-Write-Output "Chocolatey installation completed."
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install Terraform
 Write-Output "Installing Terraform..."
